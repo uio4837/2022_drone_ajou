6a3f9e35e22ea3cef73a4a42f42da86dd4abcd00
JANGJHJH <jangjh4310@ajou.ac.kr>
2022-04-16 20:14:49 +0900

    second commit

---
+++ test1.m
@@ -0,0 +1,10 @@
+KRW=sym ('KRW');
+KRW=input("원화를 입력하세요 : ")
+USD=KRW/1229.50;
+EUR=KRW/1328.84;
+JPY=KRW/9.72;
+CNY=KRW/192.62;
+fprintf("USD: %f$ EUR: %f€ JPY: %f￥ CNY: %f元",USD,EUR,JPY,CNY)
+
+
+
