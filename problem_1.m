95af5cdc50553f16820cde03f1db88ae0f3d59de
uio4837 <uio4837@naver.com>
2022-04-16 20:25:49 +0900

    problem 1,2

--- problem_1.m
+++ problem_1.m
@@ -1,8 +1,7 @@
 KRW=sym ('KRW');
 KRW=input("원화를 입력하세요 : ")
-
-USD=KRW/1229.50;
+USD=KRW/1229.50; %2022.04.16 기준 환율 적용
 EUR=KRW/1328.84;
-JPY=KRW/972.28;
+JPY=KRW/9.72;
 CNY=KRW/192.62;
-USD, EUR, JPY, CNY
+fprintf("USD: %f$ EUR: %f€ JPY: %f￥ CNY: %f元",USD,EUR,JPY,CNY)
